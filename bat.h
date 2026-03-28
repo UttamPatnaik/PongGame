a6ae0365f29fafef6f94ff70e2fe6ecaf740186c
Uttam Patnaik <uttampatnaik585@gmail.com>
2026-03-28 17:53:18 +0530

    Update bat.h

--- bat.h
+++ bat.h
@@ -5,11 +5,16 @@ class Bat
 private:
     RectangleShape m_shape;
     Vector2f m_position;
-    float m_speed = 600.f;
+    float m_speed = 600.0f;
     bool m_isMovingLeft = false;
     bool m_isMovingRight = false;
 public:
     Bat(float startX, float startY);
     FloatRect getPosition();
     RectangleShape getShape();
+    void moveLeft();
+    void moveRight();
+    void stopLeft();
+    void stopRight();
+    void update(Time dt);
 };
