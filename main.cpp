d3fe5e821aeb1fb5c081dc45eff8f054a7e99eb5
Uttam Patnaik <uttampatnaik585@gmail.com>
2026-03-28 17:53:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
-#include <SFML/Graphics.hpp>
-#include "bat.cpp"
-using namespace sf;
-
-int main()
-{
-    VideoMode vm(960, 540);
-    RenderWindow window(vm, "Pong Game!!!");
-    Bat bat(960 / 2, 540 - 20);
-    while (window.isOpen())
+    #include <SFML/Graphics.hpp>
+    #include "bat.cpp"
+    using namespace sf;
+    int main()
     {
-        Event event;
-        while (window.pollEvent(event)) 
+        VideoMode vm(960, 540);
+        RenderWindow window(vm, "Pong Game!!!");
+        Bat bat(960 / 2, 540 - 20);
+        Clock clock;
+        while (window.isOpen())
         {
-            
-            if (event.type == Event::Closed)
-                window.close(); 
+            Event event;
+            while (window.pollEvent(event)) 
+            {
+                
+                if (event.type == Event::Closed)
+                    window.close();
+                if(Keyboard::isKeyPressed(Keyboard::Left))
+                    bat.moveLeft();
+                else
+                    bat.stopLeft();
+                if(Keyboard::isKeyPressed(Keyboard::Right))
+                    bat.moveRight();
+                else
+                    bat.stopRight();
+
+            }
+            Time dt= clock.restart();
+            bat.update(dt);
+            window.clear(Color(10, 10, 18));
+            window.draw(bat.getShape());    
+            window.display();
         }
-        window.clear(Color(10, 10, 18));
-        window.draw(bat.getShape());
-        window.display();
-    } 
-    return 0;
-}
+        return 0;
+    }
